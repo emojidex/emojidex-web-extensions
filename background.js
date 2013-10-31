b2d18154a002cd9dfaa3c6f23a4b7899b51cd7b6
chikusa <yusuke1207a@gmail.com>
2013-10-31 17:10:03 +0900

    fixed option

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.browserAction.onClicked.addListener(function() {
+  chrome.extension.sendRequest({method: "getLocalStorage", key: "path"}, function(response) {
+    console.log(response.data);
+  });
   chrome.tabs.executeScript(null, {
     file: "on_click.js"
   })
--- options.js
+++ options.js
@@ -2,27 +2,20 @@ var path = document.getElementsByName("kinds");
 
 document.getElementById('save').onclick = function() {
   if (path[0].checked) {
-    localStorage['path'] = 'stylish'
-    //document.getElementById('img').innerHTML('<img src="img/Naty-cool.jpg"></img>');
+    localStorage['path'] = 'all'
   } else if (path[1].checked) {
-    localStorage['path'] = 'cool'
-  } else {
-    localStorage['path'] = 'strong'
+    localStorage['path'] = 'cssClass'
   }
 }
 
 document.body.onload = function() {
   switch(localStorage['path']) {
-    case 'stylish':
+    case 'all':
       path[0].checked = true;
       break;
 
-    case 'cool':
+    case 'cssClass':
       path[1].checked = true;
-      break;
-
-    case 'strong':
-      path[2].checked = true;
       break;
 
     default:
@@ -30,8 +23,3 @@ document.body.onload = function() {
       break;
   }
 }
-  //   localStorage['path'] = '<img src="img/Naty-stylish.jpeg"></img>'
-  // } else if (path[1].checked) {
-  //   localStorage['path'] = '<img src="img/Naty-cool.jpg"></img>'
-  // } else {
-  //   localStorage['path'] = '<img src="img/Naty-strong.jpg"></img>'
