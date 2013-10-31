454e8111ca8dc55fdf838c54d5cb7a9e8a609ecc
chikusa <yusuke1207a@gmail.com>
2013-10-31 14:40:11 +0900

    mini fix

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   var extension_ID = "chrome-extension://" + chrome.runtime.id + "/";
   var emoji_path = "img/";
   $.getJSON(extension_ID + "json/utf_emoji_by_categories_non_anime.json", function(emoji) {
-    var test = document.getElementsByClassName('emojidex_replace')[0].innerHTML.replace(/:[\-\w]+:/g, function(matched_string) {
-      var category, emojis, path, replaced;
+    var replaced_html = document.getElementsByClassName('emojidex_replace')[0].innerHTML.replace(/:[\-\w]+:/g, function(matched_string) {
+      var category, path, replaced;
       replaced = matched_string;
       for (category in emoji) {
         for (var i = 0; i < emoji[category].length; i ++) {
@@ -20,6 +20,6 @@ $(document).ready(function(){
       }
       return replaced;
     });
-    $('.emojidex_replace').empty().append(test);
+    $('.emojidex_replace').empty().append(replaced_html);
   })
 })
