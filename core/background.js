0326c642162d235fc1d3492c71874345478d6447
snowsunny <snowsunny1107@gmail.com>
2013-11-26 17:59:39 +0900

    finish: setup grunt

--- core/background.js
+++ core/background.js
@@ -1,8 +1,14 @@
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.extension.sendRequest({method: "getLocalStorage", key: "path"}, function(response) {
-    console.log(response.data);
+(function() {
+  chrome.browserAction.onClicked.addListener(function() {
+    chrome.extension.sendRequest({
+      method: "getLocalStorage",
+      key: "path"
+    }, function(response) {
+      return console.log(response.data);
+    });
+    return chrome.tabs.executeScript(null, {
+      file: "on_click.js"
+    });
   });
-  chrome.tabs.executeScript(null, {
-    file: "on_click.js"
-  })
-})
+
+}).call(this);
--- core/on_click.js
+++ core/on_click.js
@@ -1,25 +1,35 @@
-$(document).ready(function(){
-  var extension_path = "chrome-extension://" + chrome.runtime.id + "/";
-  var emoji_path = "img/";
-  $.getJSON(extension_path + "json/utf_emoji_by_categories_non_anime.json", function(emoji){
-    var replaced_html = document.getElementsByClassName('emojidex_replace')[0].innerHTML.replace(/:[\-\w]+:/g, function(matched_string){
-      var category, path, replaced;
-      replaced = matched_string;
-      for(category in emoji){
-        for(var i = 0; i < emoji[category].length; i ++){
-          matched_string = matched_string.replace(/:/g, "");
-          if(emoji[category][i].name === matched_string){
-            path = emoji_path || "";
-            if(path.length && path.charAt(path.length - 1) !== "/"){
-              path += "/";
+(function() {
+  $(document).ready(function() {
+    var emoji_path, extension_path;
+    extension_path = "chrome-extension://" + chrome.runtime.id + "/";
+    emoji_path = "img/";
+    return $.getJSON(extension_path + "json/utf_emoji_by_categories_non_anime.json", function(emoji) {
+      var replaced_html;
+      replaced_html = document.getElementsByClassName("emojidex_replace")[0].innerHTML.replace(/:[\-\w]+:/g, function(matched_string) {
+        var category, i, path, replaced;
+        category = void 0;
+        path = void 0;
+        replaced = void 0;
+        replaced = matched_string;
+        for (category in emoji) {
+          i = 0;
+          while (i < emoji[category].length) {
+            matched_string = matched_string.replace(/:/g, "");
+            if (emoji[category][i].name === matched_string) {
+              path = emoji_path || "";
+              if (path.length && path.charAt(path.length - 1) !== "/") {
+                path += "/";
+              }
+              replaced = "<img src='" + extension_path + path + matched_string + ".svg' alt='" + matched_string + "'>";
+              break;
             }
-            replaced = "<img src='" + extension_path + path + matched_string + ".svg' alt='" + matched_string + "'>";
-            break;
+            i++;
           }
         }
-      }
-      return replaced;
+        return replaced;
+      });
+      return $(".emojidex_replace").empty().append(replaced_html);
     });
-    $('.emojidex_replace').empty().append(replaced_html);
-  })
-})
+  });
+
+}).call(this);
