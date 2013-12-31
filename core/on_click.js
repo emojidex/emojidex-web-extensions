edef1ca71e9dc5d2e82a4a7f6446261b7aedcf83
snowsunny <snowsunny1107@gmail.com>
2014-01-01 02:03:38 +0900

    fixed replace

--- core/on_click.js
+++ core/on_click.js
@@ -1,66 +1,64 @@
 (function() {
   $(document).ready(function() {
-    var emoji_path, extension_path;
+    var emojis_path, extension_path;
     extension_path = "chrome-extension://" + chrome.runtime.id + "/";
-    emoji_path = "img/";
-    return $.getJSON(extension_path + "json/utf_emoji_by_categories_non_anime.json", function(emoji) {
-      var emoji_regexps;
-      emoji_regexps = Plugin.prototype.setEmojiCSS_getEmojiRegexps(emojis_data);
-      Plugin.prototype.setEmojiIcon(emojis_data, element, emoji_regexps);
-      return {
-        setEmojiCSS_getEmojiRegexps: function(emojis_data) {
-          var category, emojis_css, emojis_in_category, regexp_for_code, regexp_for_utf, _i, _len;
-          regexp_for_utf = "";
-          regexp_for_code = ":(";
-          emojis_css = $('<style type="text/css" />');
-          for (category in emojis_data) {
-            emojis_in_category = emojis_data[category];
-            for (_i = 0, _len = emojis_in_category.length; _i < _len; _i++) {
-              emoji = emojis_in_category[_i];
-              regexp_for_utf += emoji.moji + "|";
-              regexp_for_code += emoji.name + "|";
-              emojis_css.append("i.emojidex-" + emoji.moji + " {background-image: url('" + $.emojiarea.path + emoji.name + ".svg')}");
-            }
+    emojis_path = "img/";
+    return $.getJSON(extension_path + "json/utf_emoji_by_categories_non_anime.json", function(emojis_data) {
+      var emoji_regexps, setEmojiCSS_getEmojiRegexps, setEmojiIcon;
+      setEmojiCSS_getEmojiRegexps = function(emojis_data) {
+        var category, emoji, emojis_css, emojis_in_category, regexp_for_code, regexp_for_utf, _i, _len;
+        regexp_for_utf = "";
+        regexp_for_code = ":(";
+        emojis_css = $('<style type="text/css" />');
+        for (category in emojis_data) {
+          emojis_in_category = emojis_data[category];
+          for (_i = 0, _len = emojis_in_category.length; _i < _len; _i++) {
+            emoji = emojis_in_category[_i];
+            regexp_for_utf += emoji.moji + "|";
+            regexp_for_code += emoji.name + "|";
+            emojis_css.append("i.emojidex-" + emoji.moji + " {background-image: url('" + extension_path + emojis_path + emoji.name + ".svg')}");
           }
-          $("head").append(emojis_css);
-          return [regexp_for_utf.slice(0, -1), regexp_for_code.slice(0, -1) + "):"];
-        },
-        setEmojiIcon: function(emojis_data, element, emoji_regexps) {
-          var getEmojiTag, replaceForCode, replaceForUTF;
-          getEmojiTag = function(emoji_utf) {
-            return '<i class="emojidex-' + emoji_utf + '"></i>';
-          };
-          replaceForUTF = function(replaced_string, emoji_regexp) {
-            return replaced_string = replaced_string.replace(new RegExp(emoji_regexp, "g"), function(matched_string) {
-              return getEmojiTag(matched_string);
-            });
-          };
-          replaceForCode = function(replaced_string, emoji_regexp, emojis_data) {
-            return replaced_string = replaced_string.replace(new RegExp(emoji_regexp, "g"), function(matched_string) {
-              var category, _i, _len, _ref;
-              matched_string = matched_string.replace(/:/g, "");
-              for (category in emojis_data) {
-                _ref = emojis_data[category];
-                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                  emoji = _ref[_i];
-                  if (emoji.name === matched_string) {
-                    return getEmojiTag(emoji.moji);
-                  }
+        }
+        $("head").append(emojis_css);
+        return [regexp_for_utf.slice(0, -1), regexp_for_code.slice(0, -1) + "):"];
+      };
+      setEmojiIcon = function(emojis_data, emoji_regexps) {
+        var getEmojiTag, replaceForCode, replaceForUTF;
+        getEmojiTag = function(emoji_utf) {
+          return '<i class="emojidex-' + emoji_utf + '"></i>';
+        };
+        replaceForUTF = function(replaced_string, emoji_regexp) {
+          return replaced_string = replaced_string.replace(new RegExp(emoji_regexp, "g"), function(matched_string) {
+            return getEmojiTag(matched_string);
+          });
+        };
+        replaceForCode = function(replaced_string, emoji_regexp, emojis_data) {
+          return replaced_string = replaced_string.replace(new RegExp(emoji_regexp, "g"), function(matched_string) {
+            var category, emoji, _i, _len, _ref;
+            matched_string = matched_string.replace(/:/g, "");
+            for (category in emojis_data) {
+              _ref = emojis_data[category];
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                emoji = _ref[_i];
+                if (emoji.name === matched_string) {
+                  return getEmojiTag(emoji.moji);
                 }
               }
-            });
-          };
-          return $(element).find(":not(iframe,textarea,script)").andSelf().contents().filter(function() {
-            return this.nodeType === Node.TEXT_NODE;
-          }).each(function() {
-            var replaced_string;
-            replaced_string = this.textContent;
-            replaced_string = replaceForUTF(replaced_string, emoji_regexps[0]);
-            replaced_string = replaceForCode(replaced_string, emoji_regexps[1], emojis_data);
-            return $(this).replaceWith(replaced_string);
+            }
           });
-        }
+        };
+        return $("body").find(":not(iframe,textarea,script)").andSelf().contents().filter(function() {
+          return this.nodeType === Node.TEXT_NODE;
+        }).each(function() {
+          var replaced_string;
+          replaced_string = this.textContent;
+          replaced_string = replaceForUTF(replaced_string, emoji_regexps[0]);
+          replaced_string = replaceForCode(replaced_string, emoji_regexps[1], emojis_data);
+          return $(this).replaceWith(replaced_string);
+        });
       };
+      emoji_regexps = setEmojiCSS_getEmojiRegexps(emojis_data);
+      return setEmojiIcon(emojis_data, emoji_regexps);
     });
   });
 
--- src/on_click.coffee
+++ src/on_click.coffee
@@ -1,30 +1,9 @@
 $(document).ready ->
   extension_path = "chrome-extension://" + chrome.runtime.id + "/"
-  emoji_path = "img/"
-  $.getJSON extension_path + "json/utf_emoji_by_categories_non_anime.json", (emoji) ->
-    # replaced_html = document.getElementsByClassName("emojidex_replace")[0].innerHTML.replace(/:[\-\w]+:/g, (matched_string) ->
-    #   category = undefined
-    #   path = undefined
-    #   replaced = undefined
-    #   replaced = matched_string
-    #   for category of emoji
-    #     i = 0
-    #     while i < emoji[category].length
-    #       matched_string = matched_string.replace(/:/g, "")
-    #       if emoji[category][i].name is matched_string
-    #         path = emoji_path or ""
-    #         path += "/"  if path.length and path.charAt(path.length - 1) isnt "/"
-    #         replaced = "<img src='" + extension_path + path + matched_string + ".svg' alt='" + matched_string + "'>"
-    #         break
-    #       i++
-    #   replaced
-    # )
-    # $(".emojidex_replace").empty().append replaced_html
+  emojis_path = "img/"
+  $.getJSON extension_path + "json/utf_emoji_by_categories_non_anime.json", (emojis_data) ->
 
-    emoji_regexps = Plugin::setEmojiCSS_getEmojiRegexps emojis_data
-    Plugin::setEmojiIcon emojis_data, element, emoji_regexps
-
-    setEmojiCSS_getEmojiRegexps: (emojis_data) ->
+    setEmojiCSS_getEmojiRegexps = (emojis_data) ->
       regexp_for_utf = ""
       regexp_for_code = ":("
       emojis_css = $('<style type="text/css" />')
@@ -33,11 +12,11 @@ $(document).ready ->
         for emoji in emojis_in_category
           regexp_for_utf += emoji.moji + "|"
           regexp_for_code += emoji.name + "|"
-          emojis_css.append "i.emojidex-" + emoji.moji + " {background-image: url('" + $.emojiarea.path + emoji.name + ".svg')}"
+          emojis_css.append "i.emojidex-" + emoji.moji + " {background-image: url('" + extension_path + emojis_path + emoji.name + ".svg')}"
       $("head").append emojis_css
       return [regexp_for_utf.slice(0, -1), regexp_for_code.slice(0, -1) + "):"]
 
-    setEmojiIcon: (emojis_data, element, emoji_regexps) ->
+    setEmojiIcon = (emojis_data, emoji_regexps) ->
       getEmojiTag = (emoji_utf) ->
         return '<i class="emojidex-' + emoji_utf + '"></i>'
       
@@ -53,10 +32,13 @@ $(document).ready ->
               if emoji.name is matched_string
                 return getEmojiTag emoji.moji
 
-      $(element).find(":not(iframe,textarea,script)").andSelf().contents().filter(->
+      $("body").find(":not(iframe,textarea,script)").andSelf().contents().filter(->
         @nodeType is Node.TEXT_NODE
       ).each ->
         replaced_string = @textContent
         replaced_string = replaceForUTF replaced_string, emoji_regexps[0]
         replaced_string = replaceForCode replaced_string, emoji_regexps[1], emojis_data
         $(@).replaceWith replaced_string
+
+    emoji_regexps = setEmojiCSS_getEmojiRegexps emojis_data
+    setEmojiIcon emojis_data, emoji_regexps
