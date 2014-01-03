30f9273a55bd1e56ed691f4f78964b67357b842c
snowsunny <snowsunny1107@gmail.com>
2014-01-04 00:59:04 +0900

    fixed code

--- core/on_click.js
+++ core/on_click.js
@@ -4,35 +4,35 @@
     extension_path = "chrome-extension://" + chrome.runtime.id + "/";
     emojis_path = "img/";
     return $.getJSON(extension_path + "json/utf_emoji_by_categories_non_anime.json", function(emojis_data) {
-      var emoji_regexps, setEmojiCSS_getEmojiRegexps, setEmojiIcon;
+      var emoji_regexps, setEmojiCSS_getEmojiRegexps, setEmojiIcons;
       setEmojiCSS_getEmojiRegexps = function(emojis_data) {
-        var category, emoji, emojis_css, emojis_in_category, regexp_for_code, regexp_for_utf, _i, _len;
-        regexp_for_utf = "";
-        regexp_for_code = ":(";
+        var category, emoji, emojis_css, emojis_in_category, regexp_pattern_for_code, regexp_pattern_for_utf, _i, _len;
+        regexp_pattern_for_utf = "";
+        regexp_pattern_for_code = ":(";
         emojis_css = $('<style type="text/css" />');
         for (category in emojis_data) {
           emojis_in_category = emojis_data[category];
           for (_i = 0, _len = emojis_in_category.length; _i < _len; _i++) {
             emoji = emojis_in_category[_i];
-            regexp_for_utf += emoji.moji + "|";
-            regexp_for_code += emoji.name + "|";
+            regexp_pattern_for_utf += emoji.moji + "|";
+            regexp_pattern_for_code += emoji.name + "|";
             emojis_css.append("i.emojidex-" + emoji.moji + " {background-image: url('" + extension_path + emojis_path + emoji.name + ".svg')}");
           }
         }
         $("head").append(emojis_css);
-        return [regexp_for_utf.slice(0, -1), regexp_for_code.slice(0, -1) + "):"];
+        return [regexp_pattern_for_utf.slice(0, -1), regexp_pattern_for_code.slice(0, -1) + "):"];
       };
-      setEmojiIcon = function(emojis_data, emoji_regexps) {
-        var getEmojiTag, replaceForCode, replaceForUTF;
+      setEmojiIcons = function(emojis_data, emoji_regexps) {
+        var getEmojiTag, replaceForEmojiTagUsingCode, replaceForEmojiTagUsingUTF;
         getEmojiTag = function(emoji_utf) {
           return '<i class="emojidex-' + emoji_utf + '"></i>';
         };
-        replaceForUTF = function(replaced_string, emoji_regexp) {
+        replaceForEmojiTagUsingUTF = function(replaced_string, emoji_regexp) {
           return replaced_string = replaced_string.replace(new RegExp(emoji_regexp, "g"), function(matched_string) {
             return getEmojiTag(matched_string);
           });
         };
-        replaceForCode = function(replaced_string, emoji_regexp, emojis_data) {
+        replaceForEmojiTagUsingCode = function(replaced_string, emoji_regexp, emojis_data) {
           return replaced_string = replaced_string.replace(new RegExp(emoji_regexp, "g"), function(matched_string) {
             var category, emoji, _i, _len, _ref;
             matched_string = matched_string.replace(/:/g, "");
@@ -52,13 +52,13 @@
         }).each(function() {
           var replaced_string;
           replaced_string = this.textContent;
-          replaced_string = replaceForUTF(replaced_string, emoji_regexps[0]);
-          replaced_string = replaceForCode(replaced_string, emoji_regexps[1], emojis_data);
+          replaced_string = replaceForEmojiTagUsingUTF(replaced_string, emoji_regexps[0]);
+          replaced_string = replaceForEmojiTagUsingCode(replaced_string, emoji_regexps[1], emojis_data);
           return $(this).replaceWith(replaced_string);
         });
       };
       emoji_regexps = setEmojiCSS_getEmojiRegexps(emojis_data);
-      return setEmojiIcon(emojis_data, emoji_regexps);
+      return setEmojiIcons(emojis_data, emoji_regexps);
     });
   });
 
--- src/on_click.coffee
+++ src/on_click.coffee
@@ -4,27 +4,27 @@ $(document).ready ->
   $.getJSON extension_path + "json/utf_emoji_by_categories_non_anime.json", (emojis_data) ->
 
     setEmojiCSS_getEmojiRegexps = (emojis_data) ->
-      regexp_for_utf = ""
-      regexp_for_code = ":("
+      regexp_pattern_for_utf = ""
+      regexp_pattern_for_code = ":("
       emojis_css = $('<style type="text/css" />')
       for category of emojis_data
         emojis_in_category = emojis_data[category]
         for emoji in emojis_in_category
-          regexp_for_utf += emoji.moji + "|"
-          regexp_for_code += emoji.name + "|"
+          regexp_pattern_for_utf += emoji.moji + "|"
+          regexp_pattern_for_code += emoji.name + "|"
           emojis_css.append "i.emojidex-" + emoji.moji + " {background-image: url('" + extension_path + emojis_path + emoji.name + ".svg')}"
       $("head").append emojis_css
-      return [regexp_for_utf.slice(0, -1), regexp_for_code.slice(0, -1) + "):"]
+      return [regexp_pattern_for_utf.slice(0, -1), regexp_pattern_for_code.slice(0, -1) + "):"]
 
-    setEmojiIcon = (emojis_data, emoji_regexps) ->
+    setEmojiIcons = (emojis_data, emoji_regexps) ->
       getEmojiTag = (emoji_utf) ->
         return '<i class="emojidex-' + emoji_utf + '"></i>'
       
-      replaceForUTF = (replaced_string, emoji_regexp) ->
+      replaceForEmojiTagUsingUTF = (replaced_string, emoji_regexp) ->
         replaced_string = replaced_string.replace new RegExp(emoji_regexp, "g"), (matched_string) ->
           return getEmojiTag matched_string
       
-      replaceForCode = (replaced_string, emoji_regexp, emojis_data) ->
+      replaceForEmojiTagUsingCode = (replaced_string, emoji_regexp, emojis_data) ->
         replaced_string = replaced_string.replace new RegExp(emoji_regexp, "g"), (matched_string) ->
           matched_string = matched_string.replace /:/g, ""
           for category of emojis_data
@@ -36,9 +36,9 @@ $(document).ready ->
         @nodeType is Node.TEXT_NODE
       ).each ->
         replaced_string = @textContent
-        replaced_string = replaceForUTF replaced_string, emoji_regexps[0]
-        replaced_string = replaceForCode replaced_string, emoji_regexps[1], emojis_data
+        replaced_string = replaceForEmojiTagUsingUTF replaced_string, emoji_regexps[0]
+        replaced_string = replaceForEmojiTagUsingCode replaced_string, emoji_regexps[1], emojis_data
         $(@).replaceWith replaced_string
 
     emoji_regexps = setEmojiCSS_getEmojiRegexps emojis_data
-    setEmojiIcon emojis_data, emoji_regexps
+    setEmojiIcons emojis_data, emoji_regexps
