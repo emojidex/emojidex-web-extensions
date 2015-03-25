4f35d6e36e8046b2562b9359a6124fffa5d9c254
snowsunny <snowsunny1107@gmail.com>
2015-03-25 18:46:41 +0900

    fix for i18n

--- extension/js/options.js
+++ extension/js/options.js
@@ -1,8 +1,14 @@
 (function() {
   $(document).ready(function() {
-    var ls, option, options;
+    var forms, gm, ls, option, option_names, options;
+    gm = chrome.i18n.getMessage;
+    $('#header').append("<h2>" + (gm('options_header')) + "</h2>");
+    option_names = ['auto-replace', 'set-autocomplete'];
+    forms = ["<div class='checkbox'><label><input id='" + option_names[0] + "' type='checkbox'>" + (gm('options_auto_replace')) + "</label></div>", "<div class='checkbox'><label><input id='" + option_names[1] + "' type='checkbox'>" + (gm('options_set_autocomplete')) + "</label></div>"];
+    $('form#setting').append(forms);
     ls = $.localStorage;
-    options = ls.get(['auto-replace', 'set-autocomplete']);
+    options = ls.get(option_names);
+    console.dir(options);
     for (option in options) {
       if (options[option]) {
         $("#" + option)[0].checked = true;
--- src/coffee/options.coffee
+++ src/coffee/options.coffee
@@ -1,8 +1,22 @@
 $(document).ready ->
-  # $.removeAllStorages()
-  ls = $.localStorage
-  options = ls.get ['auto-replace', 'set-autocomplete']
+  gm = chrome.i18n.getMessage
 
+  $('#header').append "<h2>#{gm 'options_header'}</h2>"
+
+  option_names = [
+    'auto-replace'
+    'set-autocomplete'
+  ]
+  forms = [
+    "<div class='checkbox'><label><input id='#{option_names[0]}' type='checkbox'>#{gm 'options_auto_replace'}</label></div>"
+    "<div class='checkbox'><label><input id='#{option_names[1]}' type='checkbox'>#{gm 'options_set_autocomplete'}</label></div>"
+  ]
+
+  $('form#setting').append forms
+
+  ls = $.localStorage
+  options = ls.get option_names
+  console.dir options
   for option of options
     $("##{option}")[0].checked = true if options[option]
 
