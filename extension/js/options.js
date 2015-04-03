8fd0320aa54353e1e3802943bd508ebef4e43c49
snowsunny <snowsunny1107@gmail.com>
2015-04-03 18:27:04 +0900

    version update: v0.1.2

--- extension/js/options.js
+++ extension/js/options.js
@@ -3,12 +3,16 @@
     var forms, gm, ls, option, option_names, options;
     gm = chrome.i18n.getMessage;
     $('#header').append("<h2>" + (gm('options_header')) + "</h2>");
+    $('#licensing_info-btn').text(gm('options_nav_licensing_info'));
+    $('#licensing_info-title').text(gm('options_nav_licensing_info'));
+    $('#licensing-header-1').text(gm('options_licensing_header_1'));
+    $('#licensing-header-2').text(gm('options_licensing_header_2'));
+    $('#licensing-description').text(gm('options_licensing_description'));
     option_names = ['auto-replace', 'set-autocomplete'];
     forms = ["<div class='checkbox'><label><input id='" + option_names[0] + "' type='checkbox'>" + (gm('options_auto_replace')) + "</label></div>", "<div class='checkbox'><label><input id='" + option_names[1] + "' type='checkbox'>" + (gm('options_set_autocomplete')) + "</label></div>"];
     $('form#setting').append(forms);
     ls = $.localStorage;
     options = ls.get(option_names);
-    console.dir(options);
     for (option in options) {
       if (options[option]) {
         $("#" + option)[0].checked = true;
