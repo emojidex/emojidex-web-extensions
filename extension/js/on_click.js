0ebecbbf4ab11fe78707f395346d039b11112119
snowsunny <snowsunny1107@gmail.com>
2016-07-10 21:08:42 +0900

    fix for style load & update version v0.3.1

--- extension/js/on_click.js
+++ extension/js/on_click.js
@@ -1,11 +1,8 @@
 (function() {
   $(document).ready(function() {
-    var pallet_btn, style_path, style_tag,
+    var pallet_btn,
       _this = this;
     if ($('#emojidex-pallet-btn-crx').length === 0) {
-      style_path = chrome.extension.getURL('css/lib/emojidex.min.css');
-      style_tag = $("<link href='" + style_path + "' rel='stylesheet'>");
-      $('head').append(style_tag);
       pallet_btn = $('<span id="emojidex-pallet-btn-crx">');
       $('body').append(pallet_btn);
       return pallet_btn.emojidexPallet({
--- src/coffee/on_click.coffee
+++ src/coffee/on_click.coffee
@@ -1,8 +1,5 @@
 $(document).ready ->
   if $('#emojidex-pallet-btn-crx').length is 0
-    style_path = chrome.extension.getURL 'css/lib/emojidex.min.css'
-    style_tag = $ "<link href='#{style_path}' rel='stylesheet'>"
-    $('head').append style_tag
     pallet_btn = $ '<span id="emojidex-pallet-btn-crx">'
     $('body').append pallet_btn
     pallet_btn.emojidexPallet
